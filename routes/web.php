11ce7d5559d605f9afcbbb4ad2adfad2105ab0cf
Eduardo Pessine <eduardopessine@gmail.com>
2025-05-03 14:08:05 -0300

    feat: redirect home route to dashboard and update corresponding test

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+Route::permanentRedirect('/', '/dashboard')->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
